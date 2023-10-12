3a8a348f4e5e5e87d463cabdf2fc171ba7add934
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2023-10-12 13:59:43 +0800

    admin guard

---
+++ src/Controllers/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Brucelwayne\Blog\Coontrolelrs;
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    function index(){
+
+    }
+
+    function create(){
+
+    }
+
+    function store(){
+
+    }
+
+    function edit(){
+
+    }
+
+    function save(){
+
+    }
+}
