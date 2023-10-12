3cce5d6c6d4ac5bcfeaf6070c581fe0c8ffff46e
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2023-10-12 16:10:28 +0800

    update

--- routes/admin.php
+++ routes/admin.php
@@ -4,9 +4,19 @@ use Brucelwayne\Blog\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin/blog')->name('admin.blog.')
-    ->middleware(['web','auth:admin'])
-    ->group(function(){
+    ->middleware(['web', 'auth:admin'])
+    ->group(function () {
 
-    //admin index page
-    Route::get('/',[AdminController::class,'index'])->name('index');
-});
+        //admin index page
+        Route::get('/', [AdminController::class, 'index'])->name('index');
+
+        Route::get('create', [AdminController::class, 'index'])->name('create.show');
+
+        Route::get('edit', [AdminController::class, 'index'])->name('edit.show');
+
+        Route::delete('delete', [AdminController::class, 'index'])->name('delete');
+
+        Route::get('cates', [AdminController::class, 'index'])->name('cates.show');
+
+        Route::get('tags', [AdminController::class, 'index'])->name('tags.show');
+    });
