11e72fb3e4bb7a26297ed4db71a5c68cc38cacb4
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2023-10-15 10:59:06 +0800

    pattern

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])
-    ->group(function(){
-        Route::get('blog',[BlogController::class, 'index'])->name('blog.index');
-        Route::get('blog/{slug}', [BlogController::class, 'single'])->name('blog.single');
+    ->group(function () {
+        Route::get('blog', [BlogController::class, 'index'])->name('blog.index');
+//        Route::get('blog/page/{page?}',[BlogController::class, 'index'])->where('page','[1-9]+[0-9]*');
+        Route::get('blog/{slug}', [BlogController::class, 'single'])
+            ->name('blog.single')
+            ->where('slug', '[a-zA-Z0-9-]+');
     });
