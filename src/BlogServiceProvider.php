d86d859aa5abdd4998adda20a15d4c7e9f091f17
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2025-04-08 16:41:00 +0800

    优化视图

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -6,8 +6,6 @@ use Brucelwayne\Blog\Contracts\IBlogService;
 use Brucelwayne\Blog\Services\BlogService;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
-use Mallria\Core\Contracts\WordpressWebhookContract;
-use Mallria\Core\Services\WordpressWebhookService;
 
 class BlogServiceProvider extends ServiceProvider
 {
@@ -35,16 +33,6 @@ class BlogServiceProvider extends ServiceProvider
         });
     }
 
-    protected function loadBladeViews(): void
-    {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', $this->module_name);
-    }
-
-    protected function bootComponentNamespace(): void
-    {
-        Blade::componentNamespace('Brucelwayne\\Blog\\View\\Components', $this->module_name);
-    }
-
     protected function bootConfigs(): void
     {
         $this->mergeConfigFrom(
@@ -61,5 +49,15 @@ class BlogServiceProvider extends ServiceProvider
     protected function bootMigrations(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    protected function bootComponentNamespace(): void
+    {
+        Blade::componentNamespace('Brucelwayne\\Blog\\View\\Components', $this->module_name);
+    }
+
+    protected function loadBladeViews(): void
+    {
+//        $this->loadViewsFrom(__DIR__ . '/../resources/views', $this->module_name);
     }
 }
